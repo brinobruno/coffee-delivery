2341f6f9e243dac7527fdb22bc2d33b4085ed24c
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-01-13 20:03:10 -0300

    feat: Add react router, pages and its styled for layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 
+import { Router } from './Routes'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 // import { darkTheme } from './styles/themes/dark'
@@ -7,10 +9,12 @@ import { defaultTheme } from './styles/themes/default'
 export function App() {
   return (
     <>
-      <ThemeProvider theme={defaultTheme}>
-        <h1>Coffee Delivery</h1>
-        <GlobalStyle />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={defaultTheme}>
+          <Router />
+          <GlobalStyle />
+        </ThemeProvider>
+      </BrowserRouter>
     </>
   )
 }
