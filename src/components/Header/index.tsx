324e5ace7dac002b8962d10a8f59ef735efb3750
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-01-15 11:10:58 -0300

    fix: Change logo file depending on current theme and change icon colors based on current theme as well

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,25 @@
+import { useTheme } from 'styled-components'
 import { NavLink } from 'react-router-dom'
 import { MapPin, ShoppingCart } from 'phosphor-react'
 
+import { useThemeContext } from '../../contexts/ThemeContext'
 import { ThemeSwitcher } from './components/ThemeSwitcher'
 import { CartWrapper, HeaderContainer, MapWrapper } from './styles'
-import logo from './../../assets/logo.svg'
+
+import logoLight from './../../assets/logo-light.svg'
+import logoDark from './../../assets/logo-dark.svg'
 
 export function Header() {
+  const currentTheme = useTheme()
+  const { isDarkTheme } = useThemeContext()
+
   return (
     <HeaderContainer>
       <NavLink to="/" title="Home">
-        <img src={logo} alt="Coffee Delivery" />
+        <img
+          src={isDarkTheme === 'light' ? logoLight : logoDark}
+          alt="Coffee Delivery"
+        />
       </NavLink>
 
       <nav>
@@ -17,14 +27,14 @@ export function Header() {
 
         <NavLink to="/" title="Home">
           <MapWrapper>
-            <MapPin size={24} color="#8047F8" />
+            <MapPin size={24} color={currentTheme['purple-700']} />
             <span>São Paulo, SP</span>
           </MapWrapper>
         </NavLink>
 
         <NavLink to="/checkout" title="Checkout">
           <CartWrapper>
-            <ShoppingCart size={24} color="#C47F17" />
+            <ShoppingCart size={24} color={currentTheme['yellow-700']} />
           </CartWrapper>
         </NavLink>
       </nav>
