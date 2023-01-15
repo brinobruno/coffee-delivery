e1eaa18b5a84a8a785b65783c498ba7989aaefa3
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-01-15 11:10:16 -0300

    fix: Update icon colors and theme switching

--- src/components/Header/components/ThemeSwitcher/index.tsx
+++ src/components/Header/components/ThemeSwitcher/index.tsx
@@ -1,11 +1,22 @@
+import { useTheme } from 'styled-components'
+
+import { Moon, Sun } from 'phosphor-react'
 import { useThemeContext } from '../../../../contexts/ThemeContext'
+import { ThemeSwitchBase } from './styles'
 
 export function ThemeSwitcher() {
-  const { changeTheme } = useThemeContext()
+  const currentTheme = useTheme()
+  const { isDarkTheme, changeTheme } = useThemeContext()
 
   return (
     <>
-      <button onClick={changeTheme}>dark/light</button>
+      <ThemeSwitchBase onClick={changeTheme}>
+        {isDarkTheme === 'light' ? (
+          <Sun size={24} color={currentTheme['yellow-700']} />
+        ) : (
+          <Moon size={24} color={currentTheme['purple-700']} />
+        )}
+      </ThemeSwitchBase>
     </>
   )
 }
