47056e423dc4b101dd5fa691c408097dde2a17db
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-01-16 20:40:08 -0300

    feat: Layout for checkout page

---
+++ src/pages/Checkout/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const ProductsListContainer = styled.section`
+  min-height: calc(100vh - 6.5rem);
+`
+
+export const FormContainer = styled.form`
+  display: flex;
+  justify-content: space-between;
+  gap: 2rem;
+`
+
+export const DeliveryDetails = styled.div`
+  width: 57%;
+`
+
+export const CartContents = styled.div`
+  width: 40%;
+`
