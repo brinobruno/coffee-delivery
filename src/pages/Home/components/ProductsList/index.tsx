76228f0b9b00f0c7e0fd918fad4c184cdf7b8419
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-01-16 20:21:18 -0300

    fix: Insert <section> inside <main> tag (container)

--- src/pages/Home/components/ProductsList/index.tsx
+++ src/pages/Home/components/ProductsList/index.tsx
@@ -5,20 +5,22 @@ import { PRODUCTS_REPOSITORY } from '../../../../repository/products'
 export function ProductsList() {
   return (
     <ProductsListContainer>
-      <h2>Our coffees</h2>
+      <section>
+        <h2>Our coffees</h2>
 
-      <ProductsGrid>
-        {PRODUCTS_REPOSITORY.map((product, index) => (
-          <ProductCard
-            key={index}
-            title={product.title}
-            tags={product.tags}
-            picture={product.picture}
-            description={product.description}
-            price={product.price}
-          />
-        ))}
-      </ProductsGrid>
+        <ProductsGrid>
+          {PRODUCTS_REPOSITORY.map((product, index) => (
+            <ProductCard
+              key={index}
+              title={product.title}
+              tags={product.tags}
+              picture={product.picture}
+              description={product.description}
+              price={product.price}
+            />
+          ))}
+        </ProductsGrid>
+      </section>
     </ProductsListContainer>
   )
 }
