69b74177c59391ddcbe8d6d68f96c6647825ec12
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-01-17 20:41:37 -0300

    feat: Add form dependencies and overall basic structure - insert new styled components into it

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,18 +1,60 @@
+import { useForm } from 'react-hook-form'
+import * as zod from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
 import {
   CartContents,
+  ConfirmOrderButton,
   DeliveryDetails,
   FormContainer,
-  ProductsListContainer,
+  CheckoutContainer,
+  SelectedProductsBlock,
+  DeliveryDetailsBlock,
+  PaymentDetailsBlock,
 } from './styles'
 
-export function Checkout() {
-  return (
-    <ProductsListContainer>
-      <FormContainer>
-        <DeliveryDetails>delivery</DeliveryDetails>
+const checkoutFormValidationSchema = zod.object({
+  name: zod.string().min(2, 'Inform your name'),
+  zip: zod.number().min(8, 'Inform your ZIP code'),
+})
 
-        <CartContents>selected</CartContents>
+type CheckoutFormData = zod.infer<typeof checkoutFormValidationSchema>
+
+export function Checkout() {
+  const { /* register,  watch, */ reset, handleSubmit } =
+    useForm<CheckoutFormData>({
+      resolver: zodResolver(checkoutFormValidationSchema),
+      defaultValues: {
+        name: '',
+        zip: 0,
+      },
+    })
+
+  function handleCreateNewCheckout(data: CheckoutFormData) {
+    console.log(data)
+    reset()
+  }
+
+  // const name = watch('name')
+
+  return (
+    <CheckoutContainer>
+      <FormContainer onSubmit={handleSubmit(handleCreateNewCheckout)}>
+        <DeliveryDetails>
+          <h3>Complete your order</h3>
+
+          <DeliveryDetailsBlock>Delivery</DeliveryDetailsBlock>
+
+          <PaymentDetailsBlock>Payment</PaymentDetailsBlock>
+        </DeliveryDetails>
+
+        <CartContents>
+          <h3>Selected coffees</h3>
+          <SelectedProductsBlock>
+            <ConfirmOrderButton type="submit">Confirm order</ConfirmOrderButton>
+          </SelectedProductsBlock>
+        </CartContents>
       </FormContainer>
-    </ProductsListContainer>
+    </CheckoutContainer>
   )
 }
