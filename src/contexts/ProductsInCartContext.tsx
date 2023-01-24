a5349a1d1595426ab986357c2886da4e976b0511
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-01-24 16:17:33 -0300

    fix: Adjust base context and define interfaces

--- src/contexts/ProductsInCartContext.tsx
+++ src/contexts/ProductsInCartContext.tsx
@@ -1,26 +1,50 @@
-import { ReactNode, createContext /*, useState */ } from 'react'
+import {
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  createContext,
+  useState,
+} from 'react'
 
 interface IProductInCart {
   id: number | null
   quantity: number
 }
 
-export type ProductsInCartType = IProductInCart[]
+interface IProductsInCart {
+  itemsInCart: Array<IProductInCart>
+  setItemsInCart: Dispatch<SetStateAction<IProductInCart[]>>
+}
+
+// export type ProductsInCartType = IProductInCart[]
 
 export interface IProductsInCartProviderProps {
   children: ReactNode
 }
 
-export const ProductsInCartContext = createContext({} as ProductsInCartType)
+export const ProductsInCartContext = createContext({} as IProductsInCart)
 
 export const ProductsInCartProvider = ({
   children,
 }: IProductsInCartProviderProps) => {
-  // const [itemsInCart, setItemsInCart] = useState([])
+  const [itemsInCart, setItemsInCart] = useState<IProductInCart[]>([])
 
   return (
-    <ProductsInCartContext.Provider value={{} as ProductsInCartType}>
+    <ProductsInCartContext.Provider value={{ itemsInCart, setItemsInCart }}>
       <ProductsInCartProvider>{children}</ProductsInCartProvider>
     </ProductsInCartContext.Provider>
   )
+}
+
+export const useProductsInCartContext = () => {
+  // const { itemsInCart, setItemsInCart } = useContext(ProductsInCartContext)
+  // function handleAddItemToCart() {
+  //   if (itemsInCart === 'dark') {
+  //     setIsDarkTheme('light')
+  //   }
+  // }
+  // return {
+  //   itemsInCart,
+  //   setItemsInCart,
+  // }
 }
