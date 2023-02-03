61446d5b481123cd3198ca64676f7fa0d69c74e3
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-02-03 15:15:09 -0300

    fix: Export ErrorsContainer component

--- src/pages/Checkout/components/CheckoutForm/styles.ts
+++ src/pages/Checkout/components/CheckoutForm/styles.ts
@@ -205,3 +205,15 @@ export const DeliveryInputSmall = styled(DeliveryDetailsInputWrapper)`
     max-width: 100%;
   }
 `
+
+export const ErrorsContainer = styled.div`
+  margin-top: 2rem;
+
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  p strong {
+    color: ${(props) => props.theme['purple-500']};
+  }
+`
