7399016186a362e515d7058e30787a5dca1b38e9
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-02-06 17:59:13 -0300

    feat: Add constants file

---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+const DB_URL = 'https://viacep.com.br/ws/'
+
+const COFFEE_ITEMS_STORAGE_KEY = '@coffee-delivery:items-in-cart-1.0.0'
+
+const DELIVERY_FLAT_RATE: number = 3.5
+
+export { DB_URL, COFFEE_ITEMS_STORAGE_KEY, DELIVERY_FLAT_RATE }
